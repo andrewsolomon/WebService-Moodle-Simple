6f15e459dd9886f34b7aaa6310581b32c52a9deb
Andrew Solomon <andrew@illywhacker.net>
2014-02-28 16:35:15 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+WebService::Moodle::Simple - Blah blah blah
+
+# SYNOPSIS
+
+    use WebService::Moodle::Simple;
+
+# DESCRIPTION
+
+WebService::Moodle::Simple is
+
+# AUTHOR
+
+Andrew Solomon <andrew@geekuni.com>
+
+# COPYRIGHT
+
+Copyright 2014- Andrew Solomon
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/WebService/Moodle/Simple.pm
@@ -0,0 +1,39 @@
+package WebService::Moodle::Simple;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Moodle::Simple - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use WebService::Moodle::Simple;
+
+=head1 DESCRIPTION
+
+WebService::Moodle::Simple is
+
+=head1 AUTHOR
+
+Andrew Solomon E<lt>andrew@geekuni.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Andrew Solomon
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
